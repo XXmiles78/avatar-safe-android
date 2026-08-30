2503dfe89b87879ea3f6cccc4e8abd27b6a3a094
XXmiles78 <163579301+XXmiles78@users.noreply.github.com>
2026-08-30 17:55:45 +0200

    Add Coil for online images

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-
+    implementation("io.coil-kt.coil3:coil-compose:3.6.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.6.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
